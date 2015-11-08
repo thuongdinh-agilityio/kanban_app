27161c318f045e4443a1ea9b41635214d2cc78d1
Thuong Dinh <thuong.dinhhoang@asnet.com.vn>
2015-11-08 09:50:29 +0700

    simple code skeleton with alt and react

---
+++ app/components/App.jsx
@@ -0,0 +1,29 @@
+import uuid from 'node-uuid';
+
+import AltContainer from 'alt/AltContainer';
+import React from 'react';
+
+import Lanes from './Lanes.jsx';
+import LaneActions from '../actions/LaneActions';
+import LaneStore from '../stores/LaneStore';
+
+export default class App extends React.Component {
+  render() {
+    return ( 
+      <div>
+        <button className="add-lane" onClick={this.addItem}>+</button> 
+        <AltContainer
+            stores={[LaneStore]}
+            inject={ {
+              items: () => LaneStore.getState().lanes || [] 
+            }}
+        >
+            <Lanes />
+        </AltContainer>
+      </div>
+    );
+  }
+  addItem() {
+    LaneActions.create({name: 'New lane'});
+  }
+}
