0a58be3b2507cc0841c63fcd186e454b399beda2
Thuong Dinh <thuong.dinhhoang@asnet.com.vn>
2015-11-09 14:39:24 +0700

    Add Note test: updates notes

--- tests/note_store_test.js
+++ tests/note_store_test.js
@@ -15,4 +15,19 @@ describe('NoteStore', () => {
     assert.equal(state.notes.length, 1);
     assert.equal(state.notes[0].task, task);
   });
+
+  it('updates notes', () => { const task = 'test';
+    const updatedTask = 'test 2';
+    NoteActions.create({task});
+
+    const note = NoteStore.getState().notes[0];
+
+    NoteActions.update({...note, task: updatedTask}); 
+    
+    const state = NoteStore.getState();
+
+    assert.equal(state.notes.length, 1);
+    assert.equal(state.notes[0].task, updatedTask);
+  });
+
 });
